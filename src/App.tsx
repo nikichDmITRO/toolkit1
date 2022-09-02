a83eac49ea2c20d3fd6f9392e55e69c1ed502f30
nikichDmITRO <dmitracenkonikita5@gmail.com>
2022-09-02 21:08:58 +0300

    Знакомство с толкит

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/redusers/UseSlise';
+import { fetchUsers } from './store/redusers/ActionCreators';
 
 function App() {
+
+
+const {users,isLoading, error}=useAppSelector(state=>state.userReducer)
+const dispatch=useAppDispatch()
+useEffect(()=>{dispatch(fetchUsers())},[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {isLoading&&<h1>Идет загрузка...</h1>}
+  {error&&<h1>{error}</h1>}
+    {JSON.stringify(users,null,2)}
     </div>
   );
 }
